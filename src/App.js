435dcc33134bf2eb4f20873dda19c9fd409650e7
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2019-03-25 01:57:40 +0000

    Putting the API

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+constructor (props){
+  super(props);
+  this.state={
+    crazy: [],
+    isLoaded:false
   }
 }
+componentDidMount(){
+  fetch('https://my.api.mockaroo.com/react.json?key=b42cf6b0')
+  .then(res=>res.json())
+  .then(json=>{
+    this.setState({
+      isLoaded:true,
+      items:json
+    })
+  })
+}
+
+
+
+
+  render() {
+    var {isLoaded, items} =this.state;
+    if (!isLoaded){
+      return(
+        <div>  '...Loading...' </div>
+      )
+    }else {
+      return(
+        <div>
+        <ul>
+        {items.map(crazy =>(
+        <li className="myList"> key ={crazy.id} {crazy.first_name} |
+        Email: {crazy.email} | Avatar: {crazy.avatar}
+        </li>
+      ))};
+
+      </ul>
+
+      </div>
+    );
+    }
+    }
+  }
+
+
+
+
+
+
 
 export default App;
